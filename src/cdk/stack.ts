d78d953aacadbf2c21b896a308a3e0b4906a47c5
visionsofparadise@pm.me <visionsofparadise@pm.me>
2020-11-18 15:12:35 +0000

    listerner conditions

--- src/cdk/stack.ts
+++ src/cdk/stack.ts
@@ -6,7 +6,7 @@ import { LambdaFunction } from '@aws-cdk/aws-events-targets';
 import path from 'path';
 import { masterFunction } from 'xkore-lambda-helpers/dist/cdk/masterFunction';
 import { BlockDeviceVolume, Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, Port, UserData, Vpc } from '@aws-cdk/aws-ec2';
-import { ApplicationLoadBalancer, ApplicationProtocol } from '@aws-cdk/aws-elasticloadbalancingv2';
+import { ApplicationLoadBalancer, ApplicationProtocol, ListenerCondition } from '@aws-cdk/aws-elasticloadbalancingv2';
 import { InstanceTarget } from '@aws-cdk/aws-elasticloadbalancingv2-targets';
 import { Table } from '@aws-cdk/aws-dynamodb';
 import { RestApi, Cors, LambdaIntegration } from '@aws-cdk/aws-apigateway';
@@ -147,6 +147,9 @@ npm run startd`
 		listener.addTargets('InstanceTargets', {
 			port: 4000,
 			protocol: ApplicationProtocol.HTTP,
+			conditions: [
+				ListenerCondition.pathPatterns(['/address', '/rpc']),
+			],
 			targets: instances.map(instance => new InstanceTarget(instance)),
 			healthCheck: {
 				enabled: true
