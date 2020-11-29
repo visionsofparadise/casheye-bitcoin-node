780a32dfd0050fc264fbf13376c1abfa0ad7d649
visionsofparadise@pm.me <visionsofparadise@pm.me>
2020-11-29 05:02:29 +0000

    revert

--- src/cdk/stack.ts
+++ src/cdk/stack.ts
@@ -1,10 +1,9 @@
-import { CfnOutput, Construct, SecretValue, Stack, StackProps,  Stage, StageProps } from '@aws-cdk/core';
+import { CfnOutput, Construct, Stack, StackProps,  Stage, StageProps } from '@aws-cdk/core';
 import { serviceName } from './pipeline';
 import { BlockDeviceVolume, Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, Port, UserData, Vpc } from '@aws-cdk/aws-ec2';
 import { createOutput } from 'xkore-lambda-helpers/dist/cdk/createOutput'
 import {nanoid, customAlphabet, urlAlphabet } from 'nanoid'
 import { EventBus } from '@aws-cdk/aws-events';
-import { ARecord, PublicHostedZone, RecordTarget } from '@aws-cdk/aws-route53';
 
 const prodEC2Config = {
 	storageSize: 400,
@@ -59,34 +58,21 @@ export class CasheyeAddressWatcherStack extends Stack {
 		const config = isProd ? prodEC2Config : testEC2Config
 		const secret = nanoid()		
 		const nodeName = deploymentName + '-node-' + customAlphabet(urlAlphabet, 5)()
-		const dnsName = nodeName + '.casheye.io'
 		const shebang = `#!/bin/bash
 
 # install
 apt-get update -y
-add-apt-repository ppa:certbot/certbot -y
 apt install nodejs npm -y
-apt-get install certbot -y
-apt-get upgrade -y
-
-# ssl
-INSTANCE_DNS_NAME=${dnsName}
-certbot certonly --standalone -d $INSTANCE_DNS_NAME -n --agree-tos --email certs@casheye.io
-certbot renew --dry-run
 
 # build
 git clone https://github.com/visionsofparadise/${serviceName}.git
 cd ${serviceName}
-cp "/etc/letsencrypt/live/${dnsName}/privkey.pem" dist
-cp "/etc/letsencrypt/live/${dnsName}/fullchain.pem" dist
 npm i
 npm run test
 npm run compile
 npm i -g pm2
 STAGE=${props.STAGE} SECRET=${secret} UNIT_TEST=false pm2 start dist/index.js
-env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4000`
+env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu`
 
 		const instance = new Instance(this, 'Instance', {
 			instanceName: nodeName,
@@ -107,30 +93,15 @@ iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4
 			],
 			userData: UserData.forLinux({
 				shebang
-			}),
-			keyName: 'aws_ec2'
+			})
 		})
 
-		instance.connections.allowFromAnyIpv4(Port.tcp(22))
 		instance.connections.allowFromAnyIpv4(Port.tcp(80))
-		instance.connections.allowFromAnyIpv4(Port.tcp(443))
-		instance.connections.allowFromAnyIpv4(Port.tcp(8333))
-		instance.connections.allowFromAnyIpv4(Port.tcp(18333))
+		instance.connections.allowFromAnyIpv4(Port.tcp(isProd ? 8333 : 18333))
 
 		EventBus.grantPutEvents(instance.grantPrincipal)
 
-		const hostedZone = PublicHostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
-			zoneName: 'casheye.io',
-			hostedZoneId: SecretValue.secretsManager('CASHEYE_HOSTED_ZONE_ID').toString()
-		});
-
-		new ARecord(this, 'ARecord', {
-			zone: hostedZone,
-			target: RecordTarget.fromIpAddresses(instance.instancePublicIp),
-			recordName: nodeName
-		});
-
-		this.instanceUrl = createOutput(this, deploymentName, 'instanceUrl', 'https://' + dnsName + '/');
+		this.instanceUrl = createOutput(this, deploymentName, 'instanceUrl', 'http://' + instance.instancePublicDnsName + ':4000/');
 		this.secret = createOutput(this, deploymentName, 'secret', secret);
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { getApis } from './api'
 import { btc } from './bitcoind'
-import fs from 'fs'
-import https from 'https'
 
 const { internalApi, externalApi } = getApis(btc)
 
@@ -9,12 +7,4 @@ const internalPort = 3000
 const externalPort = 4000
 
 internalApi.listen(internalPort, () => console.log(`Internal API listening on port ${internalPort}`))
-
-const httpsServer = https.createServer({
-  key: fs.readFileSync(__dirname + '/privkey.pem', 'utf8'),
-  cert: fs.readFileSync(__dirname + '/fullchain.pem', 'utf8'),
-}, externalApi);
-
-httpsServer.listen(externalPort, () => {
-    console.log(`External API listening on port ${externalPort}`);
-});
+externalApi.listen(externalPort, () => console.log(`External API listening on port ${externalPort}`))
