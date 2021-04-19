2f5907bc605b7edb10c31a20e8415306e8e7d0f3
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-04-19 21:43:26 +0100

    test fixes

--- src/services/webhookManager/webhookManager.integration.test.ts
+++ src/services/webhookManager/webhookManager.integration.test.ts
@@ -6,8 +6,9 @@ import { sqs } from '../../sqs'
 import { testAddressGenerator } from '../../testAddressGenerator'
 import { IWebhook } from '../../types/IWebhook'
 
-describe('it sets and unsets a webhook', () => {
+describe('it sets and unsets a webhook', async () => {
 	jest.useRealTimers()
+	expect.assertions(9)
 
 	const webhook = {
 		id: kuuid.id(),
@@ -19,62 +20,54 @@ describe('it sets and unsets a webhook', () => {
 		url: 'http://localhost/'
 	}
 
-	it('sets a webhook', async () => {
-		expect.assertions(5)
-	
-		await sqs.sendMessage({
-			QueueUrl: process.env.SET_QUEUE_URL!,
-			MessageBody: JSON.stringify(webhook),
-		}).promise()
-	
-		await wait(10 * 1000)
-	
-		const redisResponse = await axios.post<IWebhook>(process.env.INSTANCE_URL! + 'redis', {
-			command: 'hget',
-			args: [webhook.address, webhook.id]
-		})
-	
-		logger.info(redisResponse.data)
-		expect(redisResponse.status).toBe(200)
-		expect(redisResponse.data).toStrictEqual(omit(webhook, ['currency']))
-	
-		const bitcoinResponse = await axios.post<{ iswatchonly: boolean; labels: string[] }>(process.env.INSTANCE_URL! + 'rpc', {
-			command: 'getAddressInfo',
-			args: [webhook.address]
-		})
-	
-		logger.info(bitcoinResponse.data)
-		expect(bitcoinResponse.status).toBe(200)
-		expect(bitcoinResponse.data.iswatchonly).toBe(true)
-		expect(bitcoinResponse.data.labels[0]).toBe('set')
-	}, 5 * 60 * 1000)
+	await sqs.sendMessage({
+		QueueUrl: process.env.SET_QUEUE_URL!,
+		MessageBody: JSON.stringify(webhook),
+	}).promise()
 
-	it('unsets a webhook', async () => {
-		expect.assertions(4)
-	
-		await sqs.sendMessage({
-			QueueUrl: process.env.UNSET_QUEUE_URL_0!,
-			MessageBody: JSON.stringify(webhook)
-		}).promise()
-	
-		await wait(10 * 1000)
-	
-		const redisResponse = await axios.post<null>(process.env.INSTANCE_URL! + 'redis', {
-			command: 'hget',
-			args: [webhook.address, webhook.id]
-		})
-	
-		logger.info(redisResponse.data)
-		expect(redisResponse.status).toBe(200)
-		expect(redisResponse.data).toBe(null)
-	
-		const bitcoinResponse = await axios.post<{ labels: string[] }>(process.env.INSTANCE_URL! + 'rpc', {
-			command: 'getAddressInfo',
-			args: [webhook.address]
-		})
-	
-		logger.info(bitcoinResponse.data)
-		expect(bitcoinResponse.status).toBe(200)
-		expect(bitcoinResponse.data.labels[0]).toBe('unset')
-	}, 5 * 60 * 1000)
+	await wait(10 * 1000)
+
+	const redisResponse1 = await axios.post<IWebhook>(process.env.INSTANCE_URL! + 'redis', {
+		command: 'hget',
+		args: [webhook.address, webhook.id]
+	})
+
+	logger.info(redisResponse1.data)
+	expect(redisResponse1.status).toBe(200)
+	expect(redisResponse1.data).toStrictEqual(omit(webhook, ['currency']))
+
+	const bitcoinResponse1 = await axios.post<{ iswatchonly: boolean; labels: Array<{ name: string; purpose: string; }> }>(process.env.INSTANCE_URL! + 'rpc', {
+		command: 'getAddressInfo',
+		args: [webhook.address]
+	})
+
+	logger.info(bitcoinResponse1.data)
+	expect(bitcoinResponse1.status).toBe(200)
+	expect(bitcoinResponse1.data.iswatchonly).toBe(true)
+	expect(bitcoinResponse1.data.labels[0].name).toBe('set')
+
+	await sqs.sendMessage({
+		QueueUrl: process.env.UNSET_QUEUE_URL_0!,
+		MessageBody: JSON.stringify(webhook)
+	}).promise()
+
+	await wait(10 * 1000)
+
+	const redisResponse2 = await axios.post<null>(process.env.INSTANCE_URL! + 'redis', {
+		command: 'hget',
+		args: [webhook.address, webhook.id]
+	})
+
+	logger.info(redisResponse2.data)
+	expect(redisResponse2.status).toBe(200)
+	expect(redisResponse2.data).toBe(null)
+
+	const bitcoinResponse2 = await axios.post<{ labels: Array<{ name: string; purpose: string; }> }>(process.env.INSTANCE_URL! + 'rpc', {
+		command: 'getAddressInfo',
+		args: [webhook.address]
+	})
+
+	logger.info(bitcoinResponse2.data)
+	expect(bitcoinResponse2.status).toBe(200)
+	expect(bitcoinResponse2.data.labels[0].name).toBe('unset')
 })
