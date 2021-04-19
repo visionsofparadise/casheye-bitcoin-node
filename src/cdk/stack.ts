bd6fb4e0a63b24675a8fe5caab4e10e5fc747c28
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-04-19 23:36:01 +0100

    add ssm iam roles

--- src/cdk/stack.ts
+++ src/cdk/stack.ts
@@ -144,6 +144,67 @@ pm2 save`
 				}),
 				userDataCausesReplacement: true
 			})
+
+			instance.addToRolePolicy(new PolicyStatement({
+				effect: Effect.ALLOW,
+				resources: ['*'],
+				actions: [
+					"ssm:DescribeAssociation",
+					"ssm:GetDeployablePatchSnapshotForInstance",
+					"ssm:GetDocument",
+					"ssm:DescribeDocument",
+					"ssm:GetManifest",
+					"ssm:GetParameter",
+					"ssm:GetParameters",
+					"ssm:ListAssociations",
+					"ssm:ListInstanceAssociations",
+					"ssm:PutInventory",
+					"ssm:PutComplianceItems",
+					"ssm:PutConfigurePackageResult",
+					"ssm:UpdateAssociationStatus",
+					"ssm:UpdateInstanceAssociationStatus",
+					"ssm:UpdateInstanceInformation"
+				]
+			}))
+
+			instance.addToRolePolicy(new PolicyStatement({
+				effect: Effect.ALLOW,
+				resources: ['*'],
+				actions: [
+					"ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel"
+				]
+			}))
+
+			instance.addToRolePolicy(new PolicyStatement({
+				effect: Effect.ALLOW,
+				resources: ['*'],
+				actions: [
+					"ec2messages:AcknowledgeMessage",
+					"ec2messages:DeleteMessage",
+					"ec2messages:FailMessage",
+					"ec2messages:GetEndpoint",
+					"ec2messages:GetMessages",
+					"ec2messages:SendReply"
+				]
+			}))
+
+			instance.addToRolePolicy(new PolicyStatement({
+				effect: Effect.ALLOW,
+				resources: ['*'],
+				actions: [
+					"cloudwatch:PutMetricData",
+					"ec2:DescribeVolumes",
+					"ec2:DescribeTags",
+					"logs:PutLogEvents",
+					"logs:DescribeLogStreams",
+					"logs:DescribeLogGroups",
+					"logs:CreateLogStream",
+					"logs:CreateLogGroup"
+				]
+			}))
 	
 			instance.connections.allowFromAnyIpv4(Port.tcp(4000))
 			instance.connections.allowFromAnyIpv4(Port.tcp(8333))
