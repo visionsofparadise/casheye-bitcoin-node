e7b61e2c3e2c9c446139aad06ed395b0f2bfd3c5
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-04-21 02:18:23 +0100

    fix url

--- src/handlers/testEndpoint.ts
+++ src/handlers/testEndpoint.ts
@@ -2,7 +2,11 @@ import { APIGatewayEvent } from 'aws-lambda/trigger/api-gateway-proxy';
 import axios from 'axios';
 import kuuid from 'kuuid';
 
-export const handler = async (event: APIGatewayEvent) => axios.post<string>(process.env.INSTANCE_URL! + 'redis', {
+export const handler = async (event: APIGatewayEvent) => {
+	await axios.post<string>(process.env.INSTANCE_URL! + 'redis', {
 		command: 'hset',
 		args: ['testData', kuuid.id(), event.body!]
 	})
+
+	return
+}
