e4da7089b17b47e36cdd8c444ff465127b77b7e6
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-04-22 22:09:30 +0100

    test fix

--- src/apiGatewaySockets.ts
+++ src/apiGatewaySockets.ts
@@ -1,7 +1,7 @@
 import AWS from "aws-sdk";
 
 export const apiGatewaySockets = new AWS.ApiGatewayManagementApi({
-	endpoint: `https://${process.env.WEBSOCKET_URL!.slice(6)}/test/@connections`,
+	endpoint: process.env.WEBSOCKET_URL!,
 	region: 'us-east-1',
 	apiVersion: '2018-11-29'
 })
