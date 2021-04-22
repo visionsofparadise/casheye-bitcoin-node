3966a237effc0d66a57838a0916d5c047dbb8f60
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-04-23 00:00:21 +0100

    test fix

--- src/tests/websocket.websocketIntegration.test.ts
+++ src/tests/websocket.websocketIntegration.test.ts
@@ -46,7 +46,7 @@ describe('integration tests', () => {
 	});
 
 	it('tests webhooks with connectionId', async () => {
-		expect.assertions(32)
+		expect.assertions(15)
 		await wait(5 * 1000)
 	
 		try {
