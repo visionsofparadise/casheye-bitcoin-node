198b2926d90ae36dfaa7f7ccea8fbc1e8a7227b0
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-04-23 20:39:29 +0100

    test fix

--- src/cdk/pipeline.ts
+++ src/cdk/pipeline.ts
@@ -70,7 +70,7 @@ export class CasheyeBitcoinNodePipelineStack extends Stack {
 			runOrder: testAppStage.nextSequentialRunOrder(),
 			additionalArtifacts: [sourceArtifact],
 			commands: [
-				'sleep 150s',
+				'sleep 180s',
 				...testEnv,
 				'npm rm bitcoind',
 				'npm ci',
