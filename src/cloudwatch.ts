ab862c3572ae1006a8334828ba5bee5f94584282
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-05-05 00:21:31 +0100

    fix region

--- src/cloudwatch.ts
+++ src/cloudwatch.ts
@@ -1,4 +1,4 @@
 import AWS from 'aws-sdk'
 
-export const cloudwatch = new AWS.CloudWatch({ apiVersion: '2010-08-01' });
-export const cloudwatchLogs = new AWS.CloudWatchLogs({ apiVersion: '2014-03-28' });
+export const cloudwatch = new AWS.CloudWatch({ apiVersion: '2010-08-01', region: 'us-east-1' });
+export const cloudwatchLogs = new AWS.CloudWatchLogs({ apiVersion: '2014-03-28', region: 'us-east-1' });
