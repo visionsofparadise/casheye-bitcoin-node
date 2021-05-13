6c397a12c96cddffbdfd4ec11231211379bb5317
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-05-13 23:27:27 +0100

    fix

--- src/services/webhookManager/setWebhook.ts
+++ src/services/webhookManager/setWebhook.ts
@@ -8,26 +8,41 @@ import { cloudLog } from '../cloudLogger/cloudLog';
 export const setWebhook = async (msg: SQS.Message): Promise<any> => {
 	const webhook = JSON.parse(msg.Body!) as IWebhook
 
+	let data: any
+
 	if (webhook.event === 'newBlock') {
 		await redis.hset('newBlock', webhook.id, encode(webhook))
 	} else {
 		await rpc.importAddress(webhook.address!, 'set', false)
-		await redis
-			.hset(webhook.address!, webhook.id, encode(webhook))
-			.catch(() => rpc.setLabel(webhook.address!, 'unset'))
+			.then(() => redis
+				.hset(webhook.address!, webhook.id, encode(webhook))
+				.catch(() => rpc.setLabel(webhook.address!, 'unset'))
+			)
+			.catch(() => {
+				data = {
+					queueEntry: {
+						Id: msg.MessageId,
+						ReceiptHandle: msg.ReceiptHandle
+					}
+				}
+			})
 	}
 
-	await cloudLog('webhook set ' + webhook.id)
+	if (!data) {
+		await cloudLog('webhook set ' + webhook.id)
 
-	return {
-		queueEntry: {
-			Id: msg.MessageId,
-			ReceiptHandle: msg.ReceiptHandle
-		},
-		eventEntry: {
-			id: webhook.id,
-			userId: webhook.userId,
-			node: parseInt(process.env.NODE_INDEX!)
-			}
+		data = {
+			queueEntry: {
+				Id: msg.MessageId,
+				ReceiptHandle: msg.ReceiptHandle
+			},
+			eventEntry: {
+				id: webhook.id,
+				userId: webhook.userId,
+				node: parseInt(process.env.NODE_INDEX!)
+				}
+		}
 	}
+
+	return data
 }
