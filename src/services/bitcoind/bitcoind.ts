9d96a80ab6b3d9b52d0f9075b65b8aca0e424a2b
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-05-20 16:58:13 +0100

    fix

--- src/services/bitcoind/bitcoind.ts
+++ src/services/bitcoind/bitcoind.ts
@@ -7,19 +7,16 @@ const rpcuser = process.env.RPC_USER || 'test';
 const rpcpassword = process.env.RPC_PASSWORD || 'test';
 
 let config: any = {
+	rpcuser,
+	rpcpassword,
+	rpcbind: "127.0.0.1",
+	rpcallowip: "127.0.0.1",
 	prune: true,
 	testnet: process.env.NETWORK === 'testnet',
 	regtest: process.env.NETWORK === 'regtest',
 	blocknotify: 'redis-cli PUBLISH new-block "%s#$(date -Ins)"',
 	walletnotify: 'redis-cli PUBLISH new-tx "%s#$(date -Ins)"',
 };
-
-if (process.env.STAGE !== 'prod') {
-	config.rpcuser = rpcuser
-	config.rpcpassword = rpcpassword
-	config.rpcbind = "127.0.0.1"
-	config.rpcallowip = "127.0.0.1"
-}
 
 export const rpc = new Client({ 
 	network: process.env.NETWORK!,
